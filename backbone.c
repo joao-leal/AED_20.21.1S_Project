ff17e7e97abd512962d5381a17ae07d1633bd9c9
joao-leal <joao.leal25@hotmail.com>
2020-12-06 16:41:18 +0000

    Revert "Prim"
    This reverts commit db7ad36043082cc02fb753b1a768f000222a2400.

--- backbone.c
+++ backbone.c
@@ -6,11 +6,6 @@
 #include "file.h"
 #include "interfaces.h"
 
-#define maxINT 1000
-
-static int fr[maxINT];
-
-
 int main(int argc, char const *argv[])
 {
     Graph *A_N = NULL; /*A_N stands for Airport Network*/ 
@@ -59,8 +54,8 @@ int main(int argc, char const *argv[])
         case 'A':
             if(mode[1] == '0')
             {
-                out_write result = {Apts, Rts, mode, v_i, 0, A1(A_N), 0 };
-                /*WriteFile(output, &result);*/
+                out_write result = {Apts, Rts, mode, v_i, 0, A0(A_N, v_i), 0 };
+                WriteFile(output, &result);
             }
             break;
 
@@ -68,7 +63,7 @@ int main(int argc, char const *argv[])
             if(mode[1] == '0')
             {
                 out_write result = {Apts, Rts, mode, v_i, v_j, 0, B0(A_N, v_i, v_j)};
-                /*WriteFile(output, &result);*/
+                WriteFile(output, &result);
                 
             }
             break;        
@@ -103,40 +98,6 @@ int main(int argc, char const *argv[])
 }
 
 
-void search(Graph *G)
-{
-
-
-}
-
-void GRAPHmstV(Graph *G, int st[], double val[])
-{
-    int v, w, min;
-    link *aux;
-
-    for( v = 0; v < G->V; v++)
-    {
-        st[v] = -1; fr[v] = v; val[v] = -1;
-    }
-
-    min = 0; st[0] = 0;
-    for(v = 0; min != G->V; v = min, st[v] = fr[v])
-    {
-        for(w = 0, min = G->V; w < G->V; w++)
-        {
-            if(st[w] == -1)
-            {
-                (G->adj[w] != NULL) ? aux = G->adj[w] : exit(0);
-                for(; aux->v != w; (aux->next != NULL) ? aux = aux->next : exit(0))
-                if(aux->wt < val[w]) {val[w] = aux->wt; fr[w] = v;}
-                if(val[w] < val[min]) min = w;
-            }
-            printf("%d ", st[w]);
-        }
-        printf("\n");
-    }
-}
-
 void BuildGraph(Graph *G, FILE *fp)
 {
     int i, v, w;
@@ -153,14 +114,17 @@ void BuildGraph(Graph *G, FILE *fp)
 }
 
 
-int A1(Graph *G)
+int A0(Graph *G, int v)
 {
-    int i, *st = (int *) calloc(G->V, sizeof(int));
-    double *val = (double *) calloc(G->V, sizeof(double));
+   /*  int i, cnt = 0;
 
-    GRAPHmstV(G, st, val);
+    for(i = 0; i < G->V; i++)
+    {
+        if( i < v-1 && G->adj[v-1][i] > 0.0) cnt++; 
+        else if(i > v-1 && G->adj[i][v-1] > 0.0) cnt++;
+    }
 
-    for(i = 0; i < G->V; i++) printf("%d ", st[i]);
+    return cnt; */
     return 0;
 }
 
